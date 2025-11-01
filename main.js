4da9bc0dcbd298e98317d0be6c1003969053de36
mob893 <111048909+mob893@users.noreply.github.com>
2025-11-01 15:59:46 +0000

    Thay đổi lớn. Tạo thêm đường dẫn đến trang blog, bài viết, và đã tạo thêm một vài mẫu. Fix thêm bug và thêm vài tính năng

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+let project = document.querySelectorAll(".project");
+console.log(project);
 
+project.forEach(element => {
+    console.log(element.id)
+    var id = element.id
+    if (id) {
+        element.addEventListener("click", (e) => {
+            window.location.href = `./${id}/${id}.html`
+        })
+    }
+});
